1498daadcf7bb032e7dfbe11d0b92f248ca452b1
Omlet144 <alexblack144@gmail.com>
2021-07-06 19:56:03 +0300

    add:
    init main

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,7 +1,16 @@
-
+#include "C_Window.h"
+#include "C_Window_Small.h"
+#include "C_Window_Big.h"
 #include <iostream>
 
 int main()
 {
-    
+	C_Window wind0(10);
+	std::cout << wind0 << "\n";
+
+	C_Window_Small wind_small(2);
+	std::cout << wind_small << "\n";
+		
+	C_Window_Big wind_big(20);
+	std::cout << wind_big << "\n";
 }
